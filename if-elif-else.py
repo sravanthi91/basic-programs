5f8955a52ae5ddf437375083a1987535a6aa2e3e
sravanthi91 <63601563+sravanthi91@users.noreply.github.com>
2021-05-17 10:51:00 +0530

    Add files via upload

---
+++ if-elif-else.py
@@ -0,0 +1,10 @@
+name=input()
+if(name=="durga"):
+    print("hii durga good morning")
+elif(name=="rama"):
+    print("hii rama good morning")
+elif(name=="harri"):
+    print("hii hari good morning")
+else:
+    print("hii guest good morning")
+    
